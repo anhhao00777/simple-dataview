be94a047ec83cd3c4cc8bce30920781db4c53bee
anhhao00777 <106489200+anhhao00777@users.noreply.github.com>
2025-02-21 16:01:13 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,72 @@
+let leftView;
+let rightView;
+document.querySelector(".top-bar").addEventListener("click", async (e)=>{
+    if(e.target.classList[1] == "-file-l"){
+        let f = await getF();
+        if(leftView) leftView.obj.remove();
+        if(f){
+            leftView = new DataTableView(document.querySelector(".inner > .lf"), f);
+        }
+    }
+    if(e.target.classList[1] == "-file-r"){
+        let f = await getF();
+        if(rightView) rightView.obj.remove();
+        if(f){
+            rightView = new DataTableView(document.querySelector(".inner > .rd"), f);
+        }
+    }
+});
+(()=>{
+    let dragElm = document.querySelector(".drag-sup");
+    let isDr = false;
+    let pos = 0;
+    window.addEventListener("dragover", (e)=>{
+        e.preventDefault();
+        dragElm.style.display = "flex";
+        isDr = true;
+        if(isDr){
+            let {x} = e;
+            if(x > (window.innerWidth/2)){
+                dragElm.style.left = "unset";
+                dragElm.style.right = "0px";
+                dragElm.querySelector(".drag-text").innerText = "Drop File On Right Side";
+                pos = 1;
+            } else{
+                dragElm.style.left = "0px";
+                dragElm.style.right = "unset";
+                dragElm.querySelector(".drag-text").innerText = "Drop File On Left Side";
+                pos = 0;
+            }
+        }
+    });
+    window.addEventListener("dragleave", (e)=>{
+        e.preventDefault();
+        dragElm.style.display = "";
+        isDr = false;
+    });
+    window.addEventListener("drop", (e)=>{
+        e.preventDefault();
+        dragElm.style.display = "";
+        isDr = false;
+        let f = e.dataTransfer.files[0];
+        if(pos == 0) {
+            if(leftView) leftView.obj.remove();
+            if(f){
+                leftView = new DataTableView(document.querySelector(".inner > .lf"), f);
+            }
+        } else if(pos == 1){
+            if(rightView) rightView.obj.remove();
+            if(f){
+                rightView = new DataTableView(document.querySelector(".inner > .rd"), f);
+            }
+        }
+    });
+})();
+
+
+
+async function getF() {
+    let h = await window.showOpenFilePicker();
+    let f = await h[0].getFile();
+    return f;
+}
